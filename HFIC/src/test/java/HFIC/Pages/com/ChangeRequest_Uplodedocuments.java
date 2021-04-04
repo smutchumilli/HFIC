ef021a1b90e46902b05cf2944627e8ea12dc5055
smutchumilli <smutchumilli@SVFL-XAADQAF-40.hfms.healthfirst.org>
2021-04-04 16:20:53 -0400

    a

--- HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
+++ HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
@@ -58,18 +58,33 @@ public class ChangeRequest_Uplodedocuments {
 			throws InterruptedException, AWTException {
 
 		Chnagerequest_Link.click();
+		Thread.sleep(3000);
 		Create_Change_Request.click();
+		Thread.sleep(5000);
+
 		Select s = new Select(CR_Type);
 		s.deselectByIndex(7);
+		Thread.sleep(3000);
+
 		grp_id.sendKeys(grpid);
 		grp_name.sendKeys(grpname);
 		grp_effectivedate.sendKeys(effdate);
 		grp_effectivedate.sendKeys(gtax);
+		Thread.sleep(3000);
+
 		search_btn.click();
+		Thread.sleep(3000);
+
 		Search_result.click();
+		Thread.sleep(3000);
+
 		DocumentUploadUI.click();
+		Thread.sleep(3000);
+
 		Select s1 = new Select(DOCtype);
 		s1.selectByIndex(1);
+		Thread.sleep(3000);
+
 		// Copy file path to clip board
 		String myString = "Q:\\Myfile.docx";
 		StringSelection stringSelection = new StringSelection(myString);
@@ -83,7 +98,7 @@ public class ChangeRequest_Uplodedocuments {
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 
 		// builder.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
 
@@ -91,7 +106,7 @@ public class ChangeRequest_Uplodedocuments {
 		rb.keyPress(KeyEvent.VK_V);
 		rb.keyRelease(KeyEvent.VK_CONTROL);
 
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_TAB);
@@ -99,17 +114,29 @@ public class ChangeRequest_Uplodedocuments {
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
 
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(3000);
+
 		// Adding Notes
 		Notes.click();
+		Thread.sleep(3000);
+
 		General_Note.click();
+		Thread.sleep(3000);
+
 		Add_Notes_Text.sendKeys(gtext);
+		Thread.sleep(3000);
+
 		Notes_Add.click();
+		Thread.sleep(3000);
+
 		OK.click();
+		Thread.sleep(3000);
+
 		Submit.click();
 
 	}
