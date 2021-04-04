00d095ca68be301f413943452b4e1548fdb8fec6
smutchumilli <smutchumilli@SVFL-XAADQAF-40.hfms.healthfirst.org>
2021-04-04 18:30:16 -0400

    CR Upload documents -4/4

--- HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
+++ HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
@@ -13,12 +13,15 @@ import org.openqa.selenium.support.ui.Select;
 
 public class ChangeRequest_Uplodedocuments {
 	WebDriver driver;
+	public Robot rb;
 
 	public ChangeRequest_Uplodedocuments(WebDriver ldriver) {
 		this.driver = ldriver;
 
 	}
 
+	@FindBy(id = "555e7955")
+	WebElement Title;
 	@FindBy(xpath = "//a[contains(text(),'Change Requests')]")
 	WebElement Chnagerequest_Link;
 	@FindBy(xpath = "//button[contains(text(),'Create Change Request')]")
@@ -39,11 +42,13 @@ public class ChangeRequest_Uplodedocuments {
 	WebElement Search_result;
 	@FindBy(name = "DocumentUploadUI_pyWorkPage_42")
 	WebElement DocumentUploadUI;
-	@FindBy(name = "253bc4da")
+	@FindBy(id = "253bc4da")
 	WebElement DOCtype;
 	@FindBy(name = "HFICCaseHeader_pyWorkPage_16")
 	WebElement Notes;
-	@FindBy(name = "//*[contains(text(),'ADD A GENERAL NOTE?')]")
+	@FindBy(name = "$PpyAttachmentPage$ppxAttachName")
+	WebElement Chosefile;
+	@FindBy(xpath = "//*[contains(text(),'ADD A GENERAL NOTE?')]")
 	WebElement General_Note;
 	@FindBy(id = "2bc4e467")
 	WebElement Add_Notes_Text;
@@ -53,91 +58,123 @@ public class ChangeRequest_Uplodedocuments {
 	WebElement OK;
 	@FindBy(xpath = "//*[contains(text(),'Submit Documents')]")
 	WebElement Submit;
+	@FindBy(id = "c5286a50Primary")
+	WebElement Other;
 
-	public void CR_Uplodedocument(String grpid, String grpname, String effdate, String gtax, String gtext)
+	public void CR_Uplodedocument_Search(String grpid, String grpname, String effdate, String gtax)
 			throws InterruptedException, AWTException {
 
 		Chnagerequest_Link.click();
 		Thread.sleep(3000);
 		Create_Change_Request.click();
-		Thread.sleep(5000);
+		Thread.sleep(3800);
 
 		Select s = new Select(CR_Type);
-		s.deselectByIndex(7);
+		s.selectByIndex(7);
 		Thread.sleep(3000);
-
 		grp_id.sendKeys(grpid);
 		grp_name.sendKeys(grpname);
 		grp_effectivedate.sendKeys(effdate);
 		grp_effectivedate.sendKeys(gtax);
 		Thread.sleep(3000);
-
 		search_btn.click();
 		Thread.sleep(3000);
-
 		Search_result.click();
-		Thread.sleep(3000);
+		Thread.sleep(3800);
+
+	}
+
+	public void DocumentType_Select(int i) throws InterruptedException, AWTException {
+		Thread.sleep(3800);
 
 		DocumentUploadUI.click();
-		Thread.sleep(3000);
-
+		Thread.sleep(3800);
 		Select s1 = new Select(DOCtype);
-		s1.selectByIndex(1);
-		Thread.sleep(3000);
-
-		// Copy file path to clip board
+		s1.selectByIndex(i);
+		Thread.sleep(3800);
 		String myString = "Q:\\Myfile.docx";
 		StringSelection stringSelection = new StringSelection(myString);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(stringSelection, null);
+
+	}
+
+	public void Rober1() throws AWTException, InterruptedException {
 		// Robert class for Upload documents
-
-		Robot rb = new Robot();
-
+		rb = new Robot();
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(3000);
-
-		// builder.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
-
+		Thread.sleep(3800);
 		rb.keyPress(KeyEvent.VK_CONTROL);
 		rb.keyPress(KeyEvent.VK_V);
+		rb.keyRelease(KeyEvent.VK_V);
 		rb.keyRelease(KeyEvent.VK_CONTROL);
-
-		Thread.sleep(3000);
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
-
-		Thread.sleep(3000);
+		Thread.sleep(3800);
 		rb.keyPress(KeyEvent.VK_TAB);
 		rb.keyRelease(KeyEvent.VK_TAB);
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(3000);
+		Thread.sleep(3800);
 
-		// Adding Notes
+	}
+
+	public void Rober2() throws AWTException, InterruptedException {
+		// Robert class for Upload documents
+		rb = new Robot();
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(3800);
+		rb.keyPress(KeyEvent.VK_CONTROL);
+		rb.keyPress(KeyEvent.VK_V);
+		rb.keyRelease(KeyEvent.VK_V);
+		rb.keyRelease(KeyEvent.VK_CONTROL);
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(3800);
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(3800);
+	}
+
+	// Adding Notes
+	public void Notes(String gtext) throws InterruptedException {
 		Notes.click();
 		Thread.sleep(3000);
-
 		General_Note.click();
 		Thread.sleep(3000);
-
 		Add_Notes_Text.sendKeys(gtext);
 		Thread.sleep(3000);
-
 		Notes_Add.click();
 		Thread.sleep(3000);
-
 		OK.click();
 		Thread.sleep(3000);
-
 		Submit.click();
+	}
+
+	public void Document_Purpose() {
+		Other.click();
+
+	}
+	public void Doc_Title(String Text) {
+		Title.sendKeys(Text);
 
 	}
 
--- HFIC/src/test/java/HFIC/Testcases/com/CR_UploadeDocuments.java
+++ HFIC/src/test/java/HFIC/Testcases/com/CR_UploadeDocuments.java
@@ -11,14 +11,57 @@ import HFIC.Pages.com.ChangeRequest_Uplodedocuments;
 public class CR_UploadeDocuments extends Baseclass {
 	@Test
 	public void CR_UploadeDocument() throws InterruptedException, AWTException {
-		
-		ChangeRequest_Uplodedocuments ChangeRequest_Uplodedocuments=PageFactory.initElements(driver, ChangeRequest_Uplodedocuments.class);
-		ChangeRequest_Uplodedocuments.CR_Uplodedocument(
-				(data.getstringdata("CR", 0, 0)),
-				(data.getstringdata("CR", 0, 1)), 
-				(data.getstringdata("CR", 0, 2)), 
-				(data.getstringdata("CR", 0, 3)), 
-				(data.getstringdata("CR", 0, 5)));
-}
+
+		ChangeRequest_Uplodedocuments ChangeRequest_Uplodedocuments = PageFactory.initElements(driver,
+				ChangeRequest_Uplodedocuments.class);
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 0, 0),
+				data.getstringdata("CR", 0, 1), data.getstringdata("CR", 0, 2), data.getstringdata("CR", 0, 3));
+		ChangeRequest_Uplodedocuments.DocumentType_Select(1);
+		ChangeRequest_Uplodedocuments.Rober1();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 0, 5));
+		// Add Second doc type
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 1, 0),
+				data.getstringdata("CR", 1, 1), data.getstringdata("CR", 1, 2), data.getstringdata("CR", 1, 3));
+		ChangeRequest_Uplodedocuments.DocumentType_Select(2);
+		ChangeRequest_Uplodedocuments.Rober1();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 1, 5));
+		// Add Third doc type
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 2, 0),
+		data.getstringdata("CR", 2, 1), data.getstringdata("CR", 2, 2), data.getstringdata("CR", 2, 3));
+		ChangeRequest_Uplodedocuments.DocumentType_Select(3);
+		ChangeRequest_Uplodedocuments.Rober2();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 2, 5));
+		// other documents
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 3, 0),
+		data.getstringdata("CR", 3, 1), data.getstringdata("CR", 3, 2), data.getstringdata("CR", 3, 3));
+		ChangeRequest_Uplodedocuments.Document_Purpose();
+		ChangeRequest_Uplodedocuments.DocumentType_Select(1);
+		ChangeRequest_Uplodedocuments.Rober2();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 3, 5));
+		//
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 4, 0),
+		data.getstringdata("CR", 4, 1), data.getstringdata("CR", 4, 2), data.getstringdata("CR", 4, 3));
+		ChangeRequest_Uplodedocuments.Document_Purpose();
+		ChangeRequest_Uplodedocuments.DocumentType_Select(2);
+		ChangeRequest_Uplodedocuments.Rober2();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 4, 5));
+		//
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 5, 0),
+		data.getstringdata("CR", 5, 1), data.getstringdata("CR", 5, 2), data.getstringdata("CR", 5, 3));
+		ChangeRequest_Uplodedocuments.Document_Purpose();
+		ChangeRequest_Uplodedocuments.DocumentType_Select(3);
+		ChangeRequest_Uplodedocuments.Rober2();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 5, 5));
+		//
+		ChangeRequest_Uplodedocuments.CR_Uplodedocument_Search(data.getstringdata("CR", 6, 0),
+		data.getstringdata("CR", 6, 1), data.getstringdata("CR", 6, 2), data.getstringdata("CR", 6, 3));
+		ChangeRequest_Uplodedocuments.Document_Purpose();
+		ChangeRequest_Uplodedocuments.DocumentType_Select(5);
+		ChangeRequest_Uplodedocuments.Rober1();
+		ChangeRequest_Uplodedocuments.Doc_Title(data.getstringdata("CR", 6, 6));
+		ChangeRequest_Uplodedocuments.Rober1();
+		ChangeRequest_Uplodedocuments.Notes(data.getstringdata("CR", 6, 5));
+
+	}
 
 }
