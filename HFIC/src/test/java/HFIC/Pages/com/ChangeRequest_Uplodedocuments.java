dc83864f6c7a0d6285f57b1071acc1dbb6360144
smutchumilli <smutchumilli@SVFL-XAADQAF-17.hfms.healthfirst.org>
2021-04-09 12:08:23 -0400

    04/09/2021-code pushed

--- HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
+++ HFIC/src/test/java/HFIC/Pages/com/ChangeRequest_Uplodedocuments.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Reporter;
 
-import com.aventstack.extentreports.Status;
 
 public class ChangeRequest_Uplodedocuments extends Baseclass {
 	WebDriver driver;
@@ -67,19 +65,19 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 	WebElement docsubtype;
 
 	public void Chnagerequest_Link() throws InterruptedException {
-		Thread.sleep(3500);
-		Chnagerequest_Link.click();
 		Thread.sleep(2000);
+		Chnagerequest_Link.click();
+		Thread.sleep(1500);
 
 	}
 
 	public void Create_Change_Request() throws InterruptedException {
-		Thread.sleep(1500);
+		Thread.sleep(1000);
 		Create_Change_Request.click();
 	}
 
 	public void CR_Type() throws InterruptedException {
-		Thread.sleep(1500);
+		Thread.sleep(500);
 
 		Select s = new Select(CR_Type);
 		s.selectByIndex(7);
@@ -105,108 +103,27 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 		Thread.sleep(1500);
 
 		search_btn.click();
-		Thread.sleep(3000);
-
-	}
-
-	public void Search_result() throws InterruptedException {
-		Search_result.click();
 		Thread.sleep(1500);
 
 	}
 
+	public void Search_result() throws InterruptedException {
+		Thread.sleep(1500);
+
+		Search_result.click();
+		Thread.sleep(1000);
+
+	}
+
 	public void Adit_DocumentType_Select(int i) throws InterruptedException, AWTException {
-		Thread.sleep(3000);
+		//Thread.sleep(1500);
 
 		Select s1 = new Select(DOCtype);
 		s1.selectByIndex(i);
 		if (i != 3) {
 
-			// Robert class for Upload documents
-			rb = new Robot();
-			Thread.sleep(4000);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(3000);
-			rb.keyPress(KeyEvent.VK_CONTROL);
-			rb.keyPress(KeyEvent.VK_V);
-			rb.keyRelease(KeyEvent.VK_V);
-			rb.keyRelease(KeyEvent.VK_CONTROL);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(3000);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(4000);
-
-		} else {
-
-			// Robert class for Upload documents
-			Thread.sleep(4000);
-			rb = new Robot();
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(3500);
-			rb.keyPress(KeyEvent.VK_CONTROL);
-			rb.keyPress(KeyEvent.VK_V);
-			rb.keyRelease(KeyEvent.VK_V);
-			rb.keyRelease(KeyEvent.VK_CONTROL);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(3500);
-			rb.keyPress(KeyEvent.VK_TAB);
-			rb.keyRelease(KeyEvent.VK_TAB);
-			rb.keyPress(KeyEvent.VK_ENTER);
-			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(3500);
-		}
-	}
-	public void Other_DocumentType_Select(int i, String s) throws InterruptedException, AWTException {
-		copy();
-
-		Thread.sleep(2000);
-
-		Select s1 = new Select(DOCtype);
-		s1.selectByIndex(i);
-		if (i == 5)
-{			Tab();
-			Doc_Title(s);
-			Tab();
-			Enter();
-			Thread.sleep(1000);
-
-			control();
-			V();
-			Tab();
-			Tab();
-			Enter();
-			Thread.sleep(1000);
-
-			Tab();
-			Enter();
-			Thread.sleep(4000);
-
-}	else if(i!=4) {
-			Tab();
 			Tab();
 			Enter();	
-			Thread.sleep(1000);
 
 			control();
 			V();
@@ -214,47 +131,106 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 			Tab();
 			Tab();
 			Enter();
-			Thread.sleep(1000);
 
 			Tab();
 			Enter();
-			Thread.sleep(4000);
 
-}else {
+
+		} else {
+
+			// Robert class for Upload documents
+			//Thread.sleep(4000);
+			Tab();
+			Tab();
+			Enter();	
+
+			control();
+			V();
+			
+			Tab();
+			Tab();
+			Enter();
+
+			Tab();
+			Enter();
+
+			//Thread.sleep(3500);
+		}
+	}
+	public void Other_DocumentType_Select(int i, String s) throws InterruptedException, AWTException {
+
+		//Thread.sleep(2000);
+
+		Select s1 = new Select(DOCtype);
+		s1.selectByIndex(i);
+		//Thread.sleep(2000);
+
+		if (i == 5)
+{			Tab();
+			Doc_Title(s);
+			Tab();
+			Enter();
+
+			control();
+			V();
+			Tab();
+			Tab();
+			Enter();
+
+			Tab();
+			Enter();
+
+}	else if (i==4){
 	Tab();
 
 	Doc_subtype(1);
 	Tab();
 	Tab();
 	Enter();
-	Thread.sleep(1000);
-
+	control();
+	V();
 	Tab();
 	Tab();
 	Enter();
-	Thread.sleep(1000);
 
 	Tab();
 	Enter();
-	Thread.sleep(4000);
 
 	
+}else  {
+	Tab();
+	Tab();
+	Enter();	
+
+	control();
+	V();
+	
+	Tab();
+	Tab();
+	Enter();
+
+	Tab();
+	Enter();
+
 }
 
 		
 	}		
 	public void Enter() throws AWTException, InterruptedException {
+		Thread.sleep(1000);
 			rb = new Robot();
 			rb.keyPress(KeyEvent.VK_ENTER);
 			rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(4000);
+			Thread.sleep(1800);
 			
 		}
 		public void Tab() throws AWTException, InterruptedException {
 			rb = new Robot();
+			Thread.sleep(500);
+
 			rb.keyPress(KeyEvent.VK_TAB);
 			rb.keyRelease(KeyEvent.VK_TAB);
-			Thread.sleep(4000);
+			Thread.sleep(1800);
 
 			
 		}
@@ -264,7 +240,7 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 		
 			rb.keyRelease(KeyEvent.VK_V);
 			rb.keyRelease(KeyEvent.VK_CONTROL);
-			Thread.sleep(4000);
+			Thread.sleep(1800);
 
 			}
 		public void control() throws AWTException, InterruptedException {
@@ -272,7 +248,7 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 
 			rb.keyPress(KeyEvent.VK_CONTROL);		
 			rb.keyPress(KeyEvent.VK_V);
-			Thread.sleep(4000);
+			Thread.sleep(1800);
 
 		}		
 		
@@ -281,7 +257,7 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 	public void DocumentUploadUI() throws InterruptedException {
 
 		DocumentUploadUI.click();
-		Thread.sleep(2500);
+		Thread.sleep(1000);
 
 	}
 
@@ -292,80 +268,68 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 		clipboard.setContents(stringSelection, null);
 	}
 
-	public void tab() throws AWTException, InterruptedException {
-		rb = new Robot();
-		rb.keyPress(KeyEvent.VK_TAB);
-		rb.keyRelease(KeyEvent.VK_TAB);
-		
-		Thread.sleep(3500);
 
-	}
-
-	public void enter() throws AWTException, InterruptedException {
-		rb = new Robot();
-		rb.keyPress(KeyEvent.VK_ENTER);
-		rb.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(3500);
-
-
-	}
 
 	public void Notes() throws InterruptedException   {
+		Thread.sleep(500);
+
 		Notes.click();
-		Thread.sleep(2000);
+		Thread.sleep(500);
 
 
 	}
 
 	public void General_Note() throws InterruptedException {
 		General_Note.click();
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 
 	}
 
 	public void Add_Notes_Text(String gtext) throws InterruptedException {
 		Add_Notes_Text.sendKeys(gtext);
-		Thread.sleep(1500);
+		//Thread.sleep(1500);
 
 	}
 
 	public void Notes_Add() throws InterruptedException {
 		Notes_Add.click();
-		Thread.sleep(2500);
+		//Thread.sleep(2500);
 
 	}
 
 	public void OK() throws InterruptedException {
 		OK.click();
-		Thread.sleep(2800);
+		//Thread.sleep(2800);
 
 	}
 
 	public void submit() throws InterruptedException {
 		Submit.click();
-		Thread.sleep(2500);
+		Thread.sleep(1000);
 
 	}
 
 	public void Document_Purpose() throws InterruptedException {
 		Other.click();
 		
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 
 
 	}
 
 	public void Doc_Title(String Text) throws InterruptedException {
 		Title.sendKeys(Text);
-		Thread.sleep(3000);
+		//Thread.sleep(1500);
 
 
 	}
 
 	public void Doc_subtype(int i) throws InterruptedException {
+		//Thread.sleep(1000);
+
 		Select n = new Select(docsubtype);
 		n.selectByIndex(i);
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 
 	}
 	
@@ -402,6 +366,7 @@ public class ChangeRequest_Uplodedocuments extends Baseclass {
 		Search_result();
 		Document_Purpose();
 		DocumentUploadUI();
+		copy();
 		Other_DocumentType_Select(i, text1);
 		Notes();
 		General_Note();
--- HFIC/src/test/java/HFIC/Testcases/com/CR_UploadeDocuments.java
+++ HFIC/src/test/java/HFIC/Testcases/com/CR_UploadeDocuments.java
@@ -11,24 +11,24 @@ import HFIC.Pages.com.Baseclass;
 import HFIC.Pages.com.ChangeRequest_Uplodedocuments;
 import Utilities.com.Helper;
 public class CR_UploadeDocuments extends Baseclass {
-//	@Test(priority=1)
-//	public void CR_Audit_UploadeDocument() throws InterruptedException, AWTException, IOException {
-//		ChangeRequest_Uplodedocuments ChangeRequest_Uplodedocuments = PageFactory.initElements(driver,ChangeRequest_Uplodedocuments.class);
-//		for(int i=1;i<=3;i++) 
-//		{
-//		test = extent.createTest(data.getstringdata("CR", i, 0));
-//		ChangeRequest_Uplodedocuments.CR_Upload_document(data.getstringdata("CR", i, 1), data.getstringdata("CR", i, 2), data.getstringdata("CR", i, 3),data.getstringdata("CR", i, 4), i, data.getstringdata("CR", i, 5));
-//		test.pass(MediaEntityBuilder.createScreenCaptureFromPath(Helper.Get_Screenshot(driver)).build());
-//		ChangeRequest_Uplodedocuments.submit();
-//		}
-//	}
+	@Test(priority=1)
+	public void CR_Audit_UploadeDocument() throws InterruptedException, AWTException, IOException {
+		ChangeRequest_Uplodedocuments ChangeRequest_Uplodedocuments = PageFactory.initElements(driver,ChangeRequest_Uplodedocuments.class);
+		for(int i=1;i<=3;i++) 
+		{
+		test = extent.createTest(data.getstringdata("CR", i, 0));
+		ChangeRequest_Uplodedocuments.CR_Upload_document(data.getstringdata("CR", i, 1), data.getstringdata("CR", i, 2), data.getstringdata("CR", i, 3),data.getstringdata("CR", i, 4), i, data.getstringdata("CR", i, 5));
+		test.pass(MediaEntityBuilder.createScreenCaptureFromPath(Helper.Get_Screenshot(driver)).build());
+		ChangeRequest_Uplodedocuments.submit();
+		}
+	}
 	@Test(priority=2)
 	public void CR_Other_UploadeDocument() throws InterruptedException, AWTException, IOException {
 		ChangeRequest_Uplodedocuments ChangeRequest_Uplodedocuments = PageFactory.initElements(driver,ChangeRequest_Uplodedocuments.class);
-		for(int i=4;i<=9;i++) 
+		for(int j=1;j<=5;j++) 
 		{
-		test = extent.createTest(data.getstringdata("CR", i, 0));
-		ChangeRequest_Uplodedocuments.CR_Upload_document_Other(data.getstringdata("CR", i, 1), data.getstringdata("CR", i, 2), data.getstringdata("CR", i, 3),data.getstringdata("CR", i, 4), i, data.getstringdata("CR", i, 6), data.getstringdata("CR", i, 5));
+		test = extent.createTest(data.getstringdata("CR", j, 7));
+		ChangeRequest_Uplodedocuments.CR_Upload_document_Other(data.getstringdata("CR", j, 8), data.getstringdata("CR", j, 9), data.getstringdata("CR", j, 10),data.getstringdata("CR", j, 11), j, data.getstringdata("CR", j, 13), data.getstringdata("CR", j, 12));
 		test.pass(MediaEntityBuilder.createScreenCaptureFromPath(Helper.Get_Screenshot(driver)).build());
 		ChangeRequest_Uplodedocuments.submit();
 
