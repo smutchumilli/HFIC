adf59b37d42efa4020e7570ba7168112c21c3c6f
smutchumilli <smutchumilli@SVFL-XAAD-081.hfms.healthfirst.org>
2021-08-25 16:57:59 -0400

    GTM Code pushed

--- HFIC/src/test/java/Pages/com/PEGA_SMG_Process.java
+++ HFIC/src/test/java/Pages/com/PEGA_SMG_Process.java
@@ -556,7 +556,6 @@ public class PEGA_SMG_Process extends Baseclass {
 
 		confirm.click();
 		Thread.sleep(1700);
-//
 		Next1.click();
 		// Add_insurance();
 		Thread.sleep(2000);
@@ -571,7 +570,7 @@ public class PEGA_SMG_Process extends Baseclass {
 		confirm.click();
 		Thread.sleep(1900);
 
-		//  
+	
 
 		mem_listing.click();
 		Thread.sleep(2000);
